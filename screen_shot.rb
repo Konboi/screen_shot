4b00ff85aeb359fb7ab93c30e6eaa473ee46531d
Konboi <ryosuke.yabuki@gmail.com>
2013-07-07 02:07:03 +0900

    Click prev button

--- screen_shot.rb
+++ screen_shot.rb
@@ -9,8 +9,31 @@ driver = Selenium::WebDriver.for :chrome
 time = Time.now
 
 driver.navigate.to "http://fonta.kayac.com/preview#picture"
+puts "wait 5"
 sleep(5)
-driver.save_screenshot("img/#{time.to_i}_picture.jpg")
+driver.save_screenshot("img/#{time.to_i}_1_picture.jpg")
+
+driver.find_element(:id, 'prev_st').click
+puts "wait 5"
+sleep(5)
+driver.save_screenshot("img/#{time.to_i}_2_picture.jpg")
+
+driver.find_element(:id, 'prev_st').click
+puts "wait 5"
+sleep(5)
+driver.find_element(:id, 'prev_st').click
+puts "wait 5"
+driver.save_screenshot("img/#{time.to_i}_3_picture.jpg")
+
+sleep(5)
+driver.find_element(:id, 'prev_st').click
+puts "wait 5"
+sleep(5)
+driver.save_screenshot("img/#{time.to_i}_4_picture.jpg")
+
+
+
+
 
 driver.navigate.to "http://konboi.com"
 sleep(2)
@@ -18,14 +41,14 @@ sleep(2)
 
 driver.navigate.to "http://fonta.kayac.com/preview#book"
 sleep(5)
-driver.save_screenshot("img/#{time.to_i}_book.jpg")
+#driver.save_screenshot("img/#{time.to_i}_book.jpg")
 
 driver.navigate.to "http://konboi.com"
 sleep(2)
 
 driver.navigate.to "http://fonta.kayac.com/preview#comic"
 sleep(5)
-driver.save_screenshot("img/#{time.to_i}_comic.jpg")
+#driver.save_screenshot("img/#{time.to_i}_comic.jpg")
 
 
 driver.quit
